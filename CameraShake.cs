b09413d04b0af835f697e5e86b7cc3844236491f
alexis <skodak95alexis@gmail.com>
2020-04-19 15:27:35 +0200

    Added cameraShake.cs

---
+++ CameraShake.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{   
+    public float shakeTimeRemaining, shakePower, shakeFadeTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+            StartShake(.2f, 1f);
+    }
+
+    void LateUpdate()
+    {
+        if(shakeTimeRemaining > 0)
+        {
+            shakeTimeRemaining -= Time.deltaTime;
+
+            float xAmount = -.25f * shakePower;
+            float yAmount = Random.Range(0f, .01f) * shakePower;
+
+            transform.position += new Vector3(xAmount, yAmount, 0f);
+
+            shakePower = Mathf.MoveTowards(shakePower, 0f, shakeFadeTime * Time.deltaTime);
+        }
+    }
+
+    void StartShake(float length, float power)
+    {
+        shakeTimeRemaining = length;
+        shakePower = power;
+        shakeFadeTime = power / length;
+    }
+}
